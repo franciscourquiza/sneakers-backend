b92411e84f97a5d887f4e3308a0befe56f1d5db5
franciscourquiza <franciscourquizalsrp@gmail.com>
2025-05-02 13:37:21 -0300

    Updated Database

--- BeIceProyect.Server/obj/Debug/net8.0/BeIceProyect.Server.AssemblyInfo.cs
+++ BeIceProyect.Server/obj/Debug/net8.0/BeIceProyect.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeIceProyect.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b83e8ae94e50da48c2c8af7d795e04e8f8459c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ef98c3ff164072bc1369261c346022ac404232")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeIceProyect.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeIceProyect.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
